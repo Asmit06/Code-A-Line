66092529d13996e99666b6847fc3353d7185a185
rajatgarg2004 <rajatgarg.bt21ele@pec.edu.in>
2023-10-15 16:35:52 +0530

    front-end-integrated-afterkalesh

--- Project/back-end/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose =  require('mongoose');
-const Users=new mongoose.Schema({
-    username:String,
-    email: String,
-    password: String
-})
-
-module.exports = mongoose.Model('users',Users);
